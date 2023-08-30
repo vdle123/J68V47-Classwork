e171c599d24d09e6a3a1ed3f7fca0c80349f53f0
30173875 <30173875@nescol.ac.uk>
2023-08-30 11:01:15 +0100

    Added HelloWorld program

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * My First Program
+ * @author Markas Krikstaponis
+ * @version 1
+ */
+public class HelloWorld {
+
+    public static void main (String[] args){
+        //Write out Hello World to the console
+        System.out.println("Hello World");
+    }
+}
