c000fac8ad8430637ea7e9e5b9274bbea1bb9dc0
30173875 <30173875@nescol.ac.uk>
2023-08-30 11:42:55 +0100

    Added HelloWorld program

---
+++ src/Exercise1.java
@@ -0,0 +1,5 @@
+public class Exercise1 {
+    public static void main (String[] args){
+        System.out.println("Whispers of the breeze,\nNature's secrets gently shared,\nPeace in every leaf.");
+    }
+}
---
+++ src/Exercise2.java
@@ -0,0 +1,9 @@
+public class Exercise2 {
+    public static void main (String[] args){
+        System.out.println("ASHE FLICK = LIFEHACKS");
+        System.out.println("ADENA BOND = ABANDONED");
+        System.out.println("CAB JACK KL = BLACKJACK");
+        System.out.println("ARIZ MEI MX = MAXIMIZER");
+        System.out.println("ADZ PARLEY = PARALYZED");
+    }
+}
