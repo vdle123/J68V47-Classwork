608ae5b870cbab393f09b57ff3a069d987b53a10
30173875 <30173875@nescol.ac.uk>
2023-09-06 11:45:56 +0100

    .

---
+++ src/Exercise2_LS2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Exercise2_LS2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number 1: ");
+        int Num1 = input.nextInt();
+        System.out.print("Enter Number 2: ");
+        int Num2 = input.nextInt();
+
+
+        System.out.println(Num1+" + "+Num2 + " = "+(Num1+Num2));
+        System.out.println(Num1+" - "+Num2 + " = "+(Num1-Num2));
+        System.out.println(Num1+" * "+Num2 + " = "+(Num1*Num2));
+        System.out.println(Num1+" / "+Num2 + " = "+(Num1/Num2));
+        System.out.println(Num1+" % "+Num2 + " = "+(Num1%Num2));
+        System.out.println(Num1+" ^ "+Num2 + " = "+(Math.pow(Num1,Num2)));
+    }
+}
---
+++ src/Exercise3_LS2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Exercise3_LS2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Item 1 cost: ");
+        float Item1_cost = input.nextFloat();
+
+        System.out.print("Enter Item 2 cost: ");
+        float Item2_cost = input.nextFloat();
+
+        System.out.print("Enter Item 3 cost: ");
+        float Item3_cost = input.nextFloat();
+
+        System.out.format("Item 1:... £%.2f\n", Item1_cost);
+        System.out.format("Item 2:... £%.2f\n", Item2_cost);
+        System.out.format("Item 3:... £%.2f\n", Item3_cost);
+        System.out.format("SUBTOTAL.."+" £%.2f",(Item1_cost+Item2_cost+Item3_cost));
+    }
+}
