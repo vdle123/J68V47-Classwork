44e79c5269516301b161b0f6df13182db836817a
vdle123 <143595998+vdle123@users.noreply.github.com>
2023-09-06 10:54:47 +0100

    Add files via upload

---
+++ src/Scanner_test.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Scanner_test {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = input.nextLine();
+    }
+}
