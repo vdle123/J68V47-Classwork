806788ad4312062dee7d41ca07727ed9cda94893
30173875 <30178375@nescol.ac.uk>
2024-01-10 11:52:13 +0000

    klsfd kkfklfzdlkfgnfsdlsdfldsklkl

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -1,46 +1,90 @@
 package Project;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static javax.swing.BoxLayout.X_AXIS;
+
 public class Main extends JFrame implements ActionListener {
 
-    JMenuBar menubar;
-    JMenu home_menu;
-    JMenu about_menu;
+    static JButton a, b, c;
+    static JFrame f;
+    static JLabel l;
+
+    JPanel buttons = new JPanel();
+    JPanel MainLabels = new JPanel();
     Main() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500,500);
-        this.setTitle("Demo");
-        this.setLocationRelativeTo(null);
-        this.setLayout(null);
+        f = new JFrame("panel");
+        l = new JLabel("");
 
-        menubar = new JMenuBar();
-        JMenu home_menu = new JMenu("HOME");
-        JMenu about_menu = new JMenu("ABOUT");
+        a = new JButton("Add Food");
+        a.setFont(new Font("Verdana",Font.BOLD,12));
+        a.setHorizontalTextPosition(SwingConstants.CENTER);
+        a.setLocation(50,50);
+        b = new JButton("Set Goals");
+        b.setFont(new Font("Verdana",Font.BOLD,12));
+        b.setHorizontalTextPosition(SwingConstants.CENTER);
 
-        this.setJMenuBar(menubar);
-        menubar.add(home_menu);
-        menubar.add(about_menu);
-        home_menu.addActionListener(this);
-        about_menu.addActionListener(this);
+        c = new JButton("Logs");
+        c.setFont(new Font("Verdana",Font.BOLD,12));
+        c.setHorizontalTextPosition(SwingConstants.CENTER);
 
-        this.setVisible(true);
+        a.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                buttons.setBackground(Color.RED);
+            }
+        });
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                buttons.setBackground(Color.BLACK);
+            }
+        });
+        c.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                buttons.setBackground(Color.BLUE);
+            }
+        });
+        buttons.setLayout(new BoxLayout(buttons, BoxLayout.Y_AXIS));
+        buttons.setBorder(new EmptyBorder(new Insets(100, 250, 150, 200)));
+
+
+        MainLabels.setLayout(new BoxLayout(MainLabels, BoxLayout.Y_AXIS));
+        MainLabels.setBorder(new EmptyBorder(new Insets(10, 175, 5, 150)));
+
+        l.setText("CALORIE TRACKER");
+        l.setFont(new Font("Verdana",Font.BOLD,14));
+        l.setForeground(Color.WHITE);
+
+        MainLabels.add(l);
+        MainLabels.setBackground(Color.BLACK);
+
+        buttons.add(a);
+        buttons.add(b);
+        buttons.add(c);
+        buttons.setBackground(Color.BLACK);
+
+        f.add(buttons, BorderLayout.EAST);
+        f.add(MainLabels, BorderLayout.NORTH);
+
+        f.setSize(500, 500);
+        f.setResizable(false);
+        f.show();
+
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new Main());
     }
 
+
     @Override
-    public void actionPerformed(ActionEvent evt) {
-        if(evt.getSource()==home_menu){
-            System.out.println("Home button");
-        }
-        else if(evt.getSource()==about_menu){
-            System.out.println("About button");
-        }
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
